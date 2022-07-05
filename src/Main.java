4a32e523aa8aa204d160c705f1fe8885827942b8
NilsK1812 <85484497+NilsK1812@users.noreply.github.com>
2022-07-05 22:57:52 +0200

    Start of the Project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Willkommen zum Gladiatorrollenspiel!\n Sobald die Enter-Taste betätigt wurde geht es los!");
+
+        input.nextLine();
+
+        abstand();
+
+        System.out.println("Ok");
+    }
+
+    private static void abstand(){
+        for(int i = 0; i <= 25; i++) {
+            System.out.println("\n");
+        }
     }
 }
